9d91ec597fd5e3f35412e07cbf357e649a184519
Sandeep Pandey <sandeep@aitpl.org>
2019-06-20 12:39:37 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAMYWNPPdwIBAI0swc92ymX7hQs6bhEJyDWG0GgUu0YKYGFK3uceZC0PN5nKHZA3UVY6YL92RwZAsMZBkybxAfeB4bzM4pYOILDNHCHap7zf9PRrZCqarqAb55tMebS0Y6EM0CSWk3NRbM7ORxAZCZBxDgaY4iZByLKXBBAKhLCtqhsrphRBIz07PJjPdUdT8q0ZD',
+    FB_VERIFY_TOKEN: 'thisismypassword',
+    FB_APP_SECRET: '963ed8baaa6a0ce9be5f71c4939de026',
+    API_AI_CLIENT_ACCESS_TOKEN: '9ac7a8df61f64a698aeaeab94ceffc55',
+    SERVER_URL: 'https://saiestatebot.herokuapp.com/',
+    GOOGLE_PROJECT_ID: 'saiestatebot-enstoa',
+    DF_LANGUAGE_CODE: 'en-US',
+    PG_CONFIG: {
+        host: 'sai-estate.com',
+        user: 'seccpl_botuser',
+        password: '#ZIhLDN4FWOY',
+        database: 'seccpl_botdb'
+    }
+};
